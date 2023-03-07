d1e39441561ce59d0dfce3d5f162161ddba423d2
Kameramann5 <d.lisachyov2013@yandex.ru>
2023-03-07 12:40:51 +0300

    Add files via upload

---
+++ group_product/component.php
@@ -0,0 +1,11 @@
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+$arResult["NAME"] = $arParams["NAME"];
+$arResult["LASTNAME"] = $arParams["LASTNAME"];
+$arResult["LASTNAME2"] = $arParams["LASTNAME2"];
+$arResult["LASTNAME3"] = $arParams["LASTNAME3"];
+
+
+
+$this->IncludeComponentTemplate(); 
+?>
