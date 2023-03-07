a52f52c3e93eda008c706ea963a0ca448427012a
Kameramann5 <d.lisachyov2013@yandex.ru>
2023-03-07 12:42:07 +0300

    Create .description.php

---
+++ group_product/.description.php
@@ -0,0 +1,13 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentDescription = array(
+	"NAME" => "Группа товаров",
+	"DESCRIPTION" => "Группа товаров",
+	"PATH" => array(
+		"ID" => "infodesign",
+	
+	),
+);
+
+?>
