4956d0336a6f28717b2067f647f99dfff0785a53
Kameramann5 <d.lisachyov2013@yandex.ru>
2023-03-07 12:44:49 +0300

    Create .parameters.php

---
+++ img.slider/lang/ru/.parameters.php
@@ -0,0 +1,15 @@
+<?
+$MESS["IBLOCK_TYPE"] = "Тип инфоблока";
+$MESS["IBLOCK_IBLOCK"] = "ID инфоблока";
+$MESS["CODE_LINK"] = "Код свойства с ссылкой";
+$MESS["DESC_ASC"] = "По возрастанию";
+$MESS["DESC_DESC"] = "По убыванию";
+$MESS["DESC_FID"] = "ID";
+$MESS["DESC_FNAME"] = "Название";
+$MESS["DESC_FACT"] = "Дата начала активности";
+$MESS["DESC_FSORT"] = "Сортировка";
+$MESS["DESC_FTSAMP"] = "Дата последнего изменения";
+$MESS["DESC_IBORD1"] = "Поле для первой сортировки новостей";
+$MESS["DESC_IBBY1"] = "Направление для первой сортировки новостей";
+?>
+
